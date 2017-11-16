d5647bad2af72efe12bab05d7d39b4f45037f271
Alaba mustapha <alabamustapha@gmail.com>
2017-11-16 14:41:48 +0000

    role management

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,24 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('admin', 'AdminController@index')->name('admin');
 
-Route::get('admin/categories', 'CategoryController@index')->name('admin_categories');
-Route::post('admin/categories', 'CategoryController@store')->name('add_category');
-Route::put('admin/categories/{category}', "CategoryController@update")->name('update_category');
-Route::get('admin/categories/{category}/edit', "CategoryController@edit")->name('edit_category');
-Route::delete('admin/categories/{category}/delete', "CategoryController@delete")->name('delete_category');
 
-Route::get('/admin/skills', 'SkillController@index')->name('admin_skills');
-Route::post('admin/skills', 'SkillController@store')->name('add_skill');
-Route::get('admin/skills/{skill}/edit', "SkillController@edit")->name('edit_skill');
-Route::delete('admin/skills/{skill}/delete', "SkillController@delete")->name('delete_skill');
+
+Route::group(['middleware' => ['admin']], function () {
+	Route::get('admin', 'AdminController@index')->name('admin');    
+    Route::get('admin/categories', 'CategoryController@index')->name('admin_categories');
+	Route::post('admin/categories', 'CategoryController@store')->name('add_category');
+	Route::put('admin/categories/{category}', "CategoryController@update")->name('update_category');
+	Route::get('admin/categories/{category}/edit', "CategoryController@edit")->name('edit_category');
+	Route::delete('admin/categories/{category}/delete', "CategoryController@delete")->name('delete_category');
+
+	Route::get('/admin/skills', 'SkillController@index')->name('admin_skills');
+	Route::post('admin/skills', 'SkillController@store')->name('add_skill');
+	Route::get('admin/skills/{skill}/edit', "SkillController@edit")->name('edit_skill');
+	Route::delete('admin/skills/{skill}/delete', "SkillController@delete")->name('delete_skill');
+
+});
+
 
 
 Route::get('/user/{user}', 'ProfileController@show')->name('profile');
